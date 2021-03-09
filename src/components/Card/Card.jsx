a2dc2669d53fb4c7fcfbddb20d06f2ac870c06c0
FlorianGazeau <gazeau.florian@gmail.com>
2021-03-09 22:33:34 +0100

    card css

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Card.scss'
 
 const Card = () => {
+
+  const [ShowModal, setShowModal] = useState(false)
+
+
+  const handleAction = () => {
+    setShowModal(prevShowModal => !prevShowModal)
+    console.log(ShowModal)
+  }
+
   return (
     <div className="card">
-      <img className="thumbnail" src="https://image.tmdb.org/t/p/original//6KErczPBROQty7QoIsaa6wJYXZi.jpg" alt=""/>
+      <div className="card__wrapper">
+        <img className="thumbnail" src="https://image.tmdb.org/t/p/original//6KErczPBROQty7QoIsaa6wJYXZi.jpg" alt=""/>
+        <span className="btn-plus" onClick={handleAction}><i className="fas fa-plus-circle"></i></span>
+        <div className={ShowModal ? 'modal active' : 'modal'}>
+          <div><button>Vu</button></div>
+          <div><button>A Voir</button></div>
+          <div><button>Fermer</button></div>
+        </div>
+      </div>
     </div>
   )
 }
